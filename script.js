134bbf3a8ecfb34ff3cb39d8d359c8823baa2dcc
Aaron Oguntoye <aaronoguntoye5925@hotmail.com>
2023-12-19 22:53:02 +0000

    added variables and the returns

--- script.js
+++ script.js
@@ -1,118 +1,35 @@
-// Array of special characters to be included in password
-const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
 
-// Array of numeric characters to be included in password
+const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
 const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters to be included in password
-const lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+const lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Array of uppercase characters to be included in password
-const upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+const upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let passwordLength = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
 
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Please enter a valid length between 8 and 128:"));
+  }
+
+  const includeLowerCase = confirm("Include lowercase characters?");
+  const includeUpperCase = confirm("Include uppercase characters?");
+  const includeNumbers = confirm("Include numeric characters?");
+  const includeSpecialChars = confirm("Include special characters?");
+
+  if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialChars) {
+    alert("At least one character type should be selected!");
+    return null;
+  }
+
+  return {
+    passwordLength: passwordLength,
+    includeLowerCase: includeLowerCase,
+    includeUpperCase: includeUpperCase,
+    includeNumbers: includeNumbers,
+    includeSpecialChars: includeSpecialChars
+  };
 }
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
-}
-
-// Get references to the #generate element
-const generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
